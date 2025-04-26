3269244c8bcf224552556526bf7b108a82f27d1b
Sakshi Rawat <142661104+Sakshirawat360@users.noreply.github.com>
2025-04-26 05:30:11 +0000

    Fixed missing Tailwind config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite'; // Make sure you import this if you're using it
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-})
+  plugins: [
+    react(),
+    tailwindcss(), // Add this if you are using the Tailwind plugin for Vite
+  ],
+});
